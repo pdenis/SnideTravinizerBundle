0fad5a3c79c905560defa6f0d1da70a2af50af1c
Pascal DENIS <pascal.denis@businessdecision.com>
2014-05-18 22:04:40 +0200

    insight helper test

---
+++ Tests/Helper/InsightHelperTest.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the SnideTravinizer bundle.
+ *
+ * (c) Pascal DENIS <pascal.denis.75@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Snide\Bundle\TravinizerBundle\Tests\Helper;
+
+use Snide\Bundle\TravinizerBundle\Helper\InsightHelper;
+use Snide\Bundle\TravinizerBundle\Model\Repo;
+
+/**
+ * Class TravisLoaderTest
+ *
+ * @author Pascal DENIS <pascal.denis.75@gmail.com>
+ */
+class InsightHelperTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var InsightHelper
+     */
+    protected $object;
+
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp()
+    {
+        $this->object = new InsightHelper();
+    }
+
+    public function testGetUrl()
+    {
+        $url = 'https://insight.sensiolabs.com/projects/aSimpleHash';
+        $this->assertEquals($url, $this->object->getUrl('aSimpleHash'));
+    }
+
+    public function testGetBadgeUrl()
+    {
+        $url = 'https://insight.sensiolabs.com/projects/aSimpleHash/mini.png';
+        $this->assertEquals($url, $this->object->getBadgeUrl('aSimpleHash', 'mini'));
+    }
+}
